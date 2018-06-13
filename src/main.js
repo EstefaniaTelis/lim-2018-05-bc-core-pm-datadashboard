cefaa764496742e1af6befd240c02d8db4df6cee
ycespedes <cespedesyavet@gmail.com>
2018-06-13 18:01:17 -0500

    cambios main

--- src/main.js
+++ src/main.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-fetch ("http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/progress.json" )
-        .then(function(progress){
-        return progress.json();
-        })
-        .then(function(progress){
-        console.log(progress);
-        });
-        
-fetch ("http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json" )
-        .then(function(users){
-        return users.json();
-        })
-        .then(function(users){
-        console.log(users);
-        });
-=======
-=======
->>>>>>> 872e13723add278ee6d0ce61a340c7b6db258340
 //Jalando data de progress JSON
 fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
     .then(function(progress) {
@@ -45,10 +25,4 @@ fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json')
     })
     .then(function(users) {
         console.log(users);
-    
-<<<<<<< HEAD
     });
->>>>>>> 3d62a3fecf1cb61b6d11508d4b8882cdab1d85ed
-=======
-    });
->>>>>>> 872e13723add278ee6d0ce61a340c7b6db258340
