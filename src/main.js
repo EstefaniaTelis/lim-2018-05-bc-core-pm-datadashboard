4a9dc0471a1e2e1655148a30de56c806502acde2
estefaniatelis <estefaniatelis@gmail.com>
2018-06-15 14:28:43 -0500

    cambios ya imprime data users

--- src/main.js
+++ src/main.js
@@ -1,28 +1,54 @@
-//Jalando data de progress JSON
-fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
-    .then(function(progress) {
-        return progress.json();
-    })
-    .then(function(progress) {
-        console.log(progress);
-    
-    });
+let dropdown = document.getElementById('cohortsDropdown');
+dropdown.length = 0;
 
-//Jalando data de cohorts JSON
-fetch('http://127.0.0.1:5500/data/cohorts.json')
-    .then(function(cohorts) {
-        return cohorts.json();
-    })
-    .then(function(cohorts) {
-        console.log(cohorts);
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Selecciona una cohort';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+fetch('http://127.0.0.1:5500/data/cohorts.json')  
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Oops, parece que hubo un problema. Status Code: ' + 
+          response.status);  
+        return;  
+      }
+ 
+      response.json().then(function(data) {  
+        let option;
     
-    });
-    
-    //Jalando data de users JSON
-fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json')
-    .then(function(users) {
-        return users.json();
-    })
-    .then(function(users) {
-        console.log(users);
-    });
+    	for (let i = 0; i < data.length; i++) {
+          option = document.createElement('option');
+      	  option.text = data[i].id;
+      	  option.value = data[i].id;
+      	  dropdown.add(option);
+    	}    
+      }); 
+    }  
+  )  
+
+  .catch(function(err) {  
+    console.error('Fetch Error -', err);  
+  });
+
+function dataUsers(){
+   var lim= document.getElementById('cohortsDropdown').value;
+   console.log(lim);
+   if(lim === "lim-2018-03-pre-core-pw")
+   {
+     fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json')
+     .then(function(users) {
+       return users.json();
+     })
+     .then((dataUsers2)=>{
+       for(var i=0;i<dataUsers2.length;i++){
+         const p=document.createElement('p');
+         p.innerText=dataUsers2[i].name;
+         six.appendChild(p);
+       }
+     }
+    )
+   }
+ }
